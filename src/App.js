82f654f38c1124956d07b0634d3bf47f225c528d
Hkc-1673 <habibkc71@gmail.com>
2021-01-05 00:41:26 +0200

    Completed adding cart process

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import './App.css';
 import { Route, BrowserRouter as Router, } from "react-router-dom";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import { connect } from "react-redux";
 
 
 
-function App() {
+function App(props) {
+
+  console.log(props.bookList)
   return (
-    <div className="app">
-      <h1>
-        Book Store
+
+      <div className="app">
+        <h1>
+          Book Store
         <img
-          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
-          alt="React Dersleri"
-        />{" "}
+            src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+            alt=""
+          />{" "}
         Let's Shopping
       </h1>
-      <Router>
-        <Route exact path="/" component={Products} />
-        <Route path="/cart" component={Cart} />
-      </Router>
+        <Router>
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={Cart} />
+        </Router>
+      </div>
 
-    </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    bookList:state.bookList
+  }
+}
+
+export default connect(mapStateToProps)(App);
