4f7c70b1ece5636d43812e1679199e4ec8558744
Hkc-1673 <habibkc71@gmail.com>
2021-01-05 00:40:00 +0200

    Completed cart design

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,37 +2,52 @@ import { Button, IconButton } from '@material-ui/core';
 import { MenuBook, RemoveShoppingCart, ShoppingCart } from '@material-ui/icons';
 import React from 'react'
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import "./Cart.css"
 
-function Cart() {
+function Cart({ cart }) {
+    const totalPrice = cart.reduce(
+        (total, item) => (total += item.price), 0);
     return (
         <div className="cart">
             <h2>
-                <Link to="/"><MenuBook color="secondary"/></Link> <span><ShoppingCart color="primary" /></span>
+                <Link to="/"><MenuBook color="secondary" /></Link> <span><ShoppingCart color="primary" /></span>
             </h2>
 
-            <h3>Total: &#8378;19.99</h3>
+            <h3>Total: &#8378;{totalPrice.toFixed(2)}</h3>
+            <p>{ cart.length }</p>
+            { cart.map((cart) => {
+                return (
+                    <div className="cart__book" key={cart.id}>
+                        <img
+                            src={cart.image}
+                            alt={cart.name}
+                        />
+                        <div>
+                            <h4>{cart.name}</h4>
+                            <p>Author: {cart.author}</p>
+                            <p>Price: &#8378;{cart.price}</p>
+                            <Button variant="contained" color="secondary" >+</Button>
+                            <IconButton>
+                                <RemoveShoppingCart color="action" />
+                            </IconButton>
+                            <Button variant="contained" color="secondary" >-</Button>
+                        </div>
+                    </div>
+                )
+            })}
 
-            <div className="cart__book">
-                <img
-                    src="https://m.media-amazon.com/images/I/5112YFsXIJL._SL230_.jpg"
-                    alt="Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future"
-                />
-                <div>
-                    <h4>Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future</h4>
-                    <p>Author: Ashlee Vance</p>
-                    <p>Price: &#8378;19.99</p>
-                    <p>Total: &#8378;19.99</p>
-                    <p>There is a book in your chart.</p>
-                    <Button variant="contained" color="secondary" >+</Button>
-                    <IconButton>
-                        <RemoveShoppingCart color="action"/>
-                    </IconButton>
-                    <Button variant="contained" color="secondary" >-</Button>
-                </div>
-            </div>
         </div>
     )
 }
 
-export default Cart
+const mapStateToProps = state => {
+    return {
+        cart: state.cart
+    }
+}
+
+
+export default connect(mapStateToProps)(Cart);
+
+
